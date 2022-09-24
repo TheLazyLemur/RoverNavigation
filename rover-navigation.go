0458790c459be93234c09a2941f6534118fbaeaf
Dan Rousseau <danrousseau@protonmail.com>
2022-09-24 15:31:56 +0200

    fix: panic when rover moves out of bounds

--- rover-navigation.go
+++ rover-navigation.go
@@ -2,55 +2,55 @@ package main
 
 import "fmt"
 
-type void struct {}
+type void struct{}
 
 var member void
 
 func main() {
-    roverA, err := CreateRover (
-        Coordinate {
-            xPos: 0,
-            yPos: 2,
-        },
-    )
-    if err != nil {
-        panic("Cannot spawn rover out of bounds")
-    }
+	roverA, err := CreateRover(
+		Coordinate{
+			xPos: 0,
+			yPos: 2,
+		},
+	)
+	if err != nil {
+		panic("Cannot spawn rover out of bounds")
+	}
 
-    roverB, err := CreateRover (
-        Coordinate {
-            xPos: 4,
-            yPos: 1,
-        },
-    )
-    if err != nil {
-        panic("Cannot spawn rover out of bounds")
-    }
+	roverB, err := CreateRover(
+		Coordinate{
+			xPos: 4,
+			yPos: 1,
+		},
+	)
+	if err != nil {
+		panic("Cannot spawn rover out of bounds")
+	}
 
-    err = roverA.ExecuteCommand("NEESSS")
-    if err != nil {
-        fmt.Println("Command puts rover out of bounds")
-    }
-    err = roverB.ExecuteCommand("WWWNNNEEE")
-    if err != nil {
-        fmt.Println("Command puts rover out of bounds")
-    }
+	err = roverA.ExecuteCommand("NEESSS")
+	if err != nil {
+		panic("Command puts rover out of bounds")
+	}
+	err = roverB.ExecuteCommand("WWWNNNEEE")
+	if err != nil {
+		panic("Command puts rover out of bounds")
+	}
 
-    GetOverlap(roverA, roverB)
+	GetOverlap(roverA, roverB)
 }
 
-func GetOverlap(roverA *Rover, roverB *Rover){
-    set := make(map[Coordinate]*void)
+func GetOverlap(roverA *Rover, roverB *Rover) {
+	set := make(map[Coordinate]*void)
 
-    for _, coordinate := range roverA.visited {
-        set[coordinate] = &member
-    }
+	for _, coordinate := range roverA.visited {
+		set[coordinate] = &member
+	}
 
-    for _, coordinate := range roverB.visited {
-        x := set[coordinate]
-        if x != nil {
-            fmt.Print("intersection at ")
-            fmt.Println(coordinate)
-        }
-    }
+	for _, coordinate := range roverB.visited {
+		x := set[coordinate]
+		if x != nil {
+			fmt.Print("intersection at ")
+			fmt.Println(coordinate)
+		}
+	}
 }
